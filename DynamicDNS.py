cb075ac078c2dea3b5160d405a9207f161ee8a4b
Kirk Gleason <kgleason@gmail.com>
2014-12-29 20:36:34 -0500

    Removed the domain data from the main script. Added an external file with the data.

--- DynamicDNS.py
+++ DynamicDNS.py
@@ -7,6 +7,7 @@
 from urllib2 import Request,urlopen
 from urllib import urlencode
 import json, os
+from config import domainData
 
 # Set the next variable to True to do some extra debugging
 debug = True
@@ -15,35 +16,36 @@ debug = True
 doApiKey = os.environ["DIGITALOCEANAPI"]
 doClientKey = os.environ["DIGITALOCEANCLIENT"]
 
-# Enter your domain information here
-domainData = {
-    "domain1.com" : {   # This is the friendly domain name. Only used for human readability.
-        "domainID" : "000000",    # The Domain ID according to Digital Ocean. See README to find this.
-        "resources" : ["#######", "#######"]   # The resource IDs that represent a specific record in this domain
-    },
-    "domain1.co" : {
-        "domainID" : "111111",
-        "resources" : ["#######", "#######"]
-    },
-    "domain1.org" : {
-        "domainID" : "222222",
-        "resources" : ["#######", "#######"]
-    },
-    "domain1.net" : {
-        "domainID" : "333333",
-        "resources" : ["#######", "#######"]
-    },
-    "domain1.us" : {
-        "domainID" : "444444",
-        "resources" : ["#######", "#######"]
-    },
-    "domain2.com" : {
-        "domainID" : "555555",
-        "resources" : ["#######", "#######"]
-    },
-}
+# Create a file called config .py with a dictionary named domainData that looks
+# like this
+#domainData = {
+#    "domain1.com" : {   # This is the friendly domain name. Only used for human readability.
+#        "domainID" : "000000",    # The Domain ID according to Digital Ocean. See README to find this.
+#        "resources" : ["#######", "#######"]   # The resource IDs that represent a specific record in this domain
+#    },
+#    "domain1.co" : {
+#        "domainID" : "111111",
+#        "resources" : ["#######", "#######"]
+#    },
+#    "domain1.org" : {
+#        "domainID" : "222222",
+#        "resources" : ["#######", "#######"]
+#    },
+#    "domain1.net" : {
+#        "domainID" : "333333",
+#        "resources" : ["#######", "#######"]
+#    },
+#    "domain1.us" : {
+#        "domainID" : "444444",
+#        "resources" : ["#######", "#######"]
+#    },
+#    "domain2.com" : {
+#        "domainID" : "555555",
+#        "resources" : ["#######", "#######"]
+#    },
+#}
 
-# The URL of a Web service that returns your IP address as plaintext. 
+# The URL of a Web service that returns your IP address as plaintext.
 #
 myIpUrl = "http://bot.whatismyipaddress.com"
 
@@ -65,22 +67,22 @@ for domain in domainData.keys():
     if debug:
         print "Starting ==> {0}".format(domain)
     domainID = domainData[domain]["domainID"]
-    
-    # Iterate over all of the specific resources to be checked.    
+
+    # Iterate over all of the specific resources to be checked.
     for resource in domainData[domain]["resources"]:
         url = "https://api.digitalocean.com/domains/{0}/records/{1}?client_id={2}&api_key={3}".format(domainID,resource,doClientKey,doApiKey)
         req = Request("{0}".format(url))
         res = json.load(urlopen(req,timeout=10))
-        
+
         if debug:
             print "Digital Ocean returned ==> {0}".format(res)
-        
+
         curDNS = res['record']['data']
-        
+
         if debug:
             print "Existing DNS record ==> {0}".format(curDNS)
-        
-        # Check to see if the record needs to be updated    
+
+        # Check to see if the record needs to be updated
         if curDNS != myIP:
             if debug:
                 subdomain = res['record']['name']
@@ -88,17 +90,16 @@ for domain in domainData.keys():
                     fqdn = "{0}.{1}".format(subdomain,domain)
                 else:
                     fqdn = domain
-                
+
                 print "Updating {} from {} => {}".format(fqdn,curDNS,myIP)
-            
+
             newUrl = "https://api.digitalocean.com/domains/{0}/records/{1}/edit?client_id={2}&api_key={3}&record_type=A&data={4}".format(domainID,resource,doClientKey,doApiKey,myIP)
-            
+
             if debug:
-                print "The update URL ==> {0}".format(newUrl)    
-            
+                print "The update URL ==> {0}".format(newUrl)
+
             req = Request(newUrl)
             res = json.load(urlopen(req,timeout=10))
-            
+
             if debug:
                 print "Digital Ocean returned ==> {0}".format(res)
-            
